be2b0a60aa5a85504877490a943a9a53e6c44a7b
Cassie <yaohuanliu1126@gmail.com>
2022-06-22 23:48:58 +1000

    modify a file

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -8,7 +8,6 @@
 */
 
 {{ config(materialized='table') }}
-
 with source_data as (
 
     select 1 as id
@@ -24,4 +23,4 @@ from source_data
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+-- where id is not null and we need to get it
